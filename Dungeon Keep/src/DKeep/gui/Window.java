317edbd1a137d1f4bf06f8d5fc924af258d3156c
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2018-03-21 15:18:24 +0000

    Added images folder

--- Dungeon Keep/src/DKeep/gui/Window.java
+++ Dungeon Keep/src/DKeep/gui/Window.java
@@ -1,25 +1,18 @@
 package DKeep.gui;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 import DKeep.logic.Game;
-import DKeep.logic.Hero;
-import DKeep.logic.Ogre;
-
-import javax.swing.JComboBox;
-import javax.swing.JTextArea;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.Font;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 public class Window {
 
