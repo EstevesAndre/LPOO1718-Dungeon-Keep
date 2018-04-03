0a8b876c3eb20bea9b3d84bb55d1d1122bacf176
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2018-04-03 21:01:34 +0000

    Divided Options constructer into smaller units of code

--- Dungeon Keep/src/DKeep/gui/Options.java
+++ Dungeon Keep/src/DKeep/gui/Options.java
@@ -20,6 +20,13 @@ public class Options extends JDialog {
 	public JComboBox<Object> comboBox;
 	public JComboBox<Object> comboBox_1;
 	public char[][] map;
+	private Map gameScreen;
+	private JLabel lblDimensions;
+	private JLabel lblAddComponent;
+	private JComboBox<Object> comboBox_2;
+	private JLabel lblAtPosition;
+	private JButton btnAdd;
+	private JRadioButton rdbtnCustom;
 
 	/**
 	 * Launch the application.
@@ -29,115 +36,62 @@ public class Options extends JDialog {
 	 * Create the dialog.
 	 */
 	public Options() {
-		setTitle("Keep Level Options");
-		setBounds(100, 100, 393, 471);
-		getContentPane().setLayout(null);
+		variableInitialize();
+		boxInitialize();
+		componentsInitialize();
+		textFieldInitialize();
+		addBtnInitialize();
+		customBtnInitialize();
+		randomBtnInitialize();
+		btnSettingsInitialize();
+	}
 
-		char[][] m5 = { {'X', 'X', 'X', 'X', 'X', 'X', 'X'},
-				{'I', ' ', ' ', ' ', ' ', ' ', 'X'},
-				{'X', ' ', ' ', ' ', ' ', ' ', 'X'},
-				{'X', ' ', ' ', ' ', ' ', ' ', 'X'},
-				{'X', ' ', ' ', ' ', ' ', ' ', 'X'},
-				{'X', ' ', ' ', ' ', ' ', ' ', 'X'},
-				{'X', 'X', 'X', 'X', 'X', 'X', 'X'}};
-
-		map = m5;
-		Map gameScreen = new Map();
-
-		JLabel lblDimensions = new JLabel("Dimensions:                      by");
-		lblDimensions.setBounds(134, 23, 156, 14);
-		getContentPane().add(lblDimensions);
-
-
-		String[] vals = {"5", "6", "7", "8"};
-		comboBox = new JComboBox<Object>(vals);
-		comboBox_1 = new JComboBox<Object>(vals);
+	private void btnSettingsInitialize() {
+		rdbtnRandom.setSelected(true);
+		buttonGroup.add(rdbtnRandom);
+		rdbtnRandom.setBounds(20, 19, 109, 23);
+		getContentPane().add(rdbtnRandom);
+		buttonGroup.add(rdbtnCustom);
+		rdbtnCustom.setBounds(20, 41, 109, 23);
+		getContentPane().add(rdbtnCustom);
 		
-		comboBox.addActionListener(new ActionListener() {
+	}
+
+	private void randomBtnInitialize() {
+		rdbtnRandom = new JRadioButton("Random");
+		rdbtnRandom.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int x = Integer.parseInt((String)comboBox.getSelectedItem()) + 2;
-				int y = Integer.parseInt((String)comboBox_1.getSelectedItem()) + 2;
-				char[][] m_prov = new char[y][x];
-				
-				for(int j = 0; j < y; j++)
-				{
-					for(int i = 0; i < x; i++)
-					{
-						if(j == 0 || j == (y-1) || i == 0 || i == (x-1))
-							m_prov[j][i] = 'X';
-						else
-							m_prov[j][i] = ' ';
-					}
-				}
-				m_prov[1][0] = 'I';
-				map = m_prov;
+				comboBox.setEnabled(false);
+				comboBox_1.setEnabled(false);
+				comboBox_2.setEnabled(false);
+				textField.setEnabled(false);
+				textField_1.setEnabled(false);
+				btnAdd.setEnabled(false);
+
+			}
+		});
+		
+	}
+
+	private void customBtnInitialize() {
+		rdbtnCustom = new JRadioButton("Custom");
+		rdbtnCustom.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				comboBox.setEnabled(true);
+				comboBox_1.setEnabled(true);
+				comboBox_2.setEnabled(true);
+				textField.setEnabled(true);
+				textField_1.setEnabled(true);
+				btnAdd.setEnabled(true);
 				gameScreen.setMap(map);
 				gameScreen.repaint();
 			}
 		});
-		comboBox.setEnabled(false);
-		comboBox.setBounds(224, 19, 44, 20);
-		getContentPane().add(comboBox);
-
 		
-		comboBox_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				int x = Integer.parseInt((String)comboBox.getSelectedItem()) + 2;
-				int y = Integer.parseInt((String)comboBox_1.getSelectedItem()) + 2;
-				char[][] m_prov = new char[y][x];
-				
-				for(int j = 0; j < y; j++)
-				{
-					for(int i = 0; i < x; i++)
-					{
-						if(j == 0 || j == (y-1) || i == 0 || i == (x-1))
-							m_prov[j][i] = 'X';
-						else
-							m_prov[j][i] = ' ';
-					}
-				}
-				m_prov[1][0] = 'I';
-				map = m_prov;
-				gameScreen.setMap(map);
-				gameScreen.repaint();
-			}
-		});
-		comboBox_1.setEnabled(false);
-		comboBox_1.setBounds(288, 19, 44, 20);
-		getContentPane().add(comboBox_1);
+	}
 
-		JLabel lblAddComponent = new JLabel("Add");
-		lblAddComponent.setBounds(135, 47, 156, 14);
-		getContentPane().add(lblAddComponent);
-
-
-		String[] components = {"Hero", "Key", "Ogre", "Wall", "Blank Space"};
-		JComboBox<Object> comboBox_2 = new JComboBox<Object>(components);
-		comboBox_2.setEnabled(false);
-		comboBox_2.setBounds(224, 44, 108, 20);
-		getContentPane().add(comboBox_2);
-
-		JLabel lblAtPosition = new JLabel("at position");
-		lblAtPosition.setBounds(134, 71, 80, 14);
-		getContentPane().add(lblAtPosition);
-
-		textField = new JTextField();
-		textField.setEnabled(false);
-		textField.setBounds(224, 68, 44, 20);
-		getContentPane().add(textField);
-		textField.setColumns(10);
-
-		textField_1 = new JTextField();
-		textField_1.setEnabled(false);
-		textField_1.setColumns(10);
-		textField_1.setBounds(288, 68, 44, 20);
-		getContentPane().add(textField_1);
-
-		
-		gameScreen.setBounds(20, 98, 336, 323);
-		getContentPane().add(gameScreen);
-		
-		JButton btnAdd = new JButton("Add");
+	private void addBtnInitialize() {
+		btnAdd = new JButton("Add");
 		btnAdd.setEnabled(false);
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -196,44 +150,131 @@ public class Options extends JDialog {
 		});
 		btnAdd.setBounds(20, 67, 89, 23);
 		getContentPane().add(btnAdd);
+		
+	}
 
-		JRadioButton rdbtnCustom = new JRadioButton("Custom");
-		rdbtnCustom.addActionListener(new ActionListener() {
+	private void textFieldInitialize() {
+		textField = new JTextField();
+		textField.setEnabled(false);
+		textField.setBounds(224, 68, 44, 20);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+
+		textField_1 = new JTextField();
+		textField_1.setEnabled(false);
+		textField_1.setColumns(10);
+		textField_1.setBounds(288, 68, 44, 20);
+		getContentPane().add(textField_1);
+		
+		gameScreen.setBounds(20, 98, 336, 323);
+		getContentPane().add(gameScreen);
+		
+	}
+
+	private void componentsInitialize() {
+		lblAddComponent = new JLabel("Add");
+		lblAddComponent.setBounds(135, 47, 156, 14);
+		getContentPane().add(lblAddComponent);
+
+
+		String[] components = {"Hero", "Key", "Ogre", "Wall", "Blank Space"};
+		comboBox_2 = new JComboBox<Object>(components);
+		comboBox_2.setEnabled(false);
+		comboBox_2.setBounds(224, 44, 108, 20);
+		getContentPane().add(comboBox_2);
+
+		lblAtPosition = new JLabel("at position");
+		lblAtPosition.setBounds(134, 71, 80, 14);
+		getContentPane().add(lblAtPosition);
+		
+	}
+
+	private void boxInitialize() {
+		String[] vals = {"5", "6", "7", "8"};
+		comboBox = new JComboBox<Object>(vals);
+		comboBox_1 = new JComboBox<Object>(vals);
+		
+		addListnerBox();
+		comboBox.setEnabled(false);
+		comboBox.setBounds(224, 19, 44, 20);
+		getContentPane().add(comboBox);
+
+		addListnerBox1();
+		comboBox_1.setEnabled(false);
+		comboBox_1.setBounds(288, 19, 44, 20);
+		getContentPane().add(comboBox_1);
+		
+	}
+
+	private void addListnerBox1() {
+		comboBox_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				comboBox.setEnabled(true);
-				comboBox_1.setEnabled(true);
-				comboBox_2.setEnabled(true);
-				textField.setEnabled(true);
-				textField_1.setEnabled(true);
-				btnAdd.setEnabled(true);
+				int x = Integer.parseInt((String)comboBox.getSelectedItem()) + 2;
+				int y = Integer.parseInt((String)comboBox_1.getSelectedItem()) + 2;
+				char[][] m_prov = new char[y][x];
+				
+				for(int j = 0; j < y; j++)
+				{
+					for(int i = 0; i < x; i++)
+					{
+						if(j == 0 || j == (y-1) || i == 0 || i == (x-1))
+							m_prov[j][i] = 'X';
+						else
+							m_prov[j][i] = ' ';
+					}
+				}
+				m_prov[1][0] = 'I';
+				map = m_prov;
 				gameScreen.setMap(map);
 				gameScreen.repaint();
 			}
 		});
+		
+	}
 
-
-		rdbtnRandom = new JRadioButton("Random");
-		rdbtnRandom.addActionListener(new ActionListener() {
+	private void addListnerBox() {
+		comboBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				comboBox.setEnabled(false);
-				comboBox_1.setEnabled(false);
-				comboBox_2.setEnabled(false);
-				textField.setEnabled(false);
-				textField_1.setEnabled(false);
-				btnAdd.setEnabled(false);
-
+				int x = Integer.parseInt((String)comboBox.getSelectedItem()) + 2;
+				int y = Integer.parseInt((String)comboBox_1.getSelectedItem()) + 2;
+				char[][] m_prov = new char[y][x];
+				
+				for(int j = 0; j < y; j++)
+				{
+					for(int i = 0; i < x; i++)
+					{
+						if(j == 0 || j == (y-1) || i == 0 || i == (x-1))
+							m_prov[j][i] = 'X';
+						else
+							m_prov[j][i] = ' ';
+					}
+				}
+				m_prov[1][0] = 'I';
+				map = m_prov;
+				gameScreen.setMap(map);
+				gameScreen.repaint();
 			}
 		});
-
-		rdbtnRandom.setSelected(true);
-		buttonGroup.add(rdbtnRandom);
-		rdbtnRandom.setBounds(20, 19, 109, 23);
-		getContentPane().add(rdbtnRandom);
-		buttonGroup.add(rdbtnCustom);
-		rdbtnCustom.setBounds(20, 41, 109, 23);
-		getContentPane().add(rdbtnCustom);
-
 		
+	}
 
+	private void variableInitialize() {
+		setTitle("Keep Level Options");
+		setBounds(100, 100, 393, 471);
+		getContentPane().setLayout(null);
+
+		char[][] m5 = { {'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+				{'I', ' ', ' ', ' ', ' ', ' ', 'X'},
+				{'X', ' ', ' ', ' ', ' ', ' ', 'X'},
+				{'X', ' ', ' ', ' ', ' ', ' ', 'X'},
+				{'X', ' ', ' ', ' ', ' ', ' ', 'X'},
+				{'X', ' ', ' ', ' ', ' ', ' ', 'X'},
+				{'X', 'X', 'X', 'X', 'X', 'X', 'X'}};
+
+		map = m5;
+		gameScreen = new Map();
+		lblDimensions = new JLabel("Dimensions:                      by");
+		lblDimensions.setBounds(134, 23, 156, 14);
+		getContentPane().add(lblDimensions);
 	}
 }
